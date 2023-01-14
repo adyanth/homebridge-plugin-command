520646ba8ef23a11b8f38ae5c6197c1bd538b2a5
Adyanth Hosavalike <me@adyanth.dev>
2023-01-13 16:06:24 -0800

    :recycle: Move to class model

--- index.js
+++ index.js
@@ -1,61 +1,62 @@
 var exec = require('child_process').exec;
 
-var Accessory, Service, Characteristic, UUIDGen;
-
-function commandAccessory(log, config) {
-  log("Command Plugin Init");
-  this.log = log;
-  this.turnOnCommand = config["turn_on"];
-  this.turnOffCommand = config["turn_off"];
-  this.checkStatusCommand = config["check_status"]
-  this.name = config["name"];
-  this.currentState = false;
-  this.invertStatus = config["invert_status"];
-  this.processState();
+module.exports = function (api) {
+  api.registerAccessory("homebridge-plugin-command", "commandAccessory", CommandAccessoryPlugin);
 }
 
-commandAccessory.prototype = {};
+class CommandAccessoryPlugin {
+  constructor(log, config, api) {
+    this.log = log;
+    this.config = config;
+    this.api = api;
+    this.currentState = false;
 
-commandAccessory.prototype.identify = function (callback) {
-  this.log.debug("Identify requested!");
-  callback(); // success
-};
+    this.log.debug('Command Accessory Plugin Loaded');
 
-commandAccessory.prototype.getServices = function () {
-  return [this.service];
-}
+    // your accessory must have an AccessoryInformation service
+    this.informationService = new this.api.hap.Service.AccessoryInformation()
+      .setCharacteristic(this.api.hap.Characteristic.Manufacturer, "adyanth")
+      .setCharacteristic(this.api.hap.Characteristic.Model, config["name"]);
 
-commandAccessory.prototype.getState = function (callback) {
-  if (!this.checkStatusCommand) {
-    callback(null, this.currentState);
-    return;
+    // create a new "Switch" service
+    this.switchService = new this.api.hap.Service.Switch(this.name);
+
+    // link methods used when getting or setting the state of the service 
+    this.switchService.getCharacteristic(this.api.hap.Characteristic.On)
+      .onGet(this.getStateHandler.bind(this))   // bind to getStateHandler method below
+      .onSet(this.setStateHandler.bind(this));  // bind to setStateHandler method below
   }
-  exec(this.checkStatusCommand, function (error, stdout, stderr) {
-    // Log if needed
-  }).on('exit', code => callback(null, this.invertStatus ? code == 0 : code != 0));
-}
 
-commandAccessory.prototype.setState = function (value, callback) {
-  exec(value ? this.turnOnCommand : this.turnOffCommand, function (error, stdout, stderr) {
-    // Log if needed
-  }).on('exit', function(code) {
+  getServices() {
+    return [
+      this.informationService,
+      this.switchService,
+    ];
+  }
+
+  async getStateHandler() {
+    this.log.info(`Getting ${this.config.name} switch state`);
+
+    if (!this.config.check_status) {
+      return this.currentState;
+    }
+
+    code = await exec(this.check_status, function (error, stdout, stderr) {
+      // Log if needed
+    })
+    
+    return this.config.invert_status ? code == 0 : code != 0;
+  }
+
+  async setStateHandler(value) {
+    this.log.info(`Setting ${this.config.name} switch state to: `, value);
+    
+    code = await exec(value ? this.config.turn_on : this.config.turn_off, function (error, stdout, stderr) {
+      // Log if needed
+    })
+
     // Set state depending on whether the command exited successfully or not.
     this.currentState = value ^ (code != 0);
-    this.getState(callback);
-  });
-}
-
-commandAccessory.prototype.processState = function () {
-  this.service = new Service.Switch(this.name);
-  this.service.getCharacteristic(Characteristic.On)
-    .on('get', this.getState)
-    .on('set', this.setState);
-};
-
-module.exports = function (api) {
-  Accessory = api.platformAccessory;
-  Service = api.hap.Service;
-  Characteristic = api.hap.Characteristic;
-  UUIDGen = api.hap.uuid;
-  api.registerAccessory("homebridge-plugin-command", "commandAccessory", commandAccessory);
+    return this.currentState;
+  }
 }
