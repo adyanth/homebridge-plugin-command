6a4140bc29b75cdc109d9bad4a2e07e6bb0b2c3c
Adyanth Hosavalike <me@adyanth.dev>
2023-01-16 20:46:18 +0000

    :loud_sound: Fix logs to debug

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ class CommandAccessoryPlugin {
     this.api = api;
     this.currentState = false;
 
-    this.log.debug(`Command Accessory Plugin: ${this.config.name} Loaded`);
-
     // your accessory must have an AccessoryInformation service
     this.informationService = new this.api.hap.Service.AccessoryInformation()
       .setCharacteristic(this.api.hap.Characteristic.Manufacturer, "adyanth")
@@ -26,6 +24,8 @@ class CommandAccessoryPlugin {
     this.switchService.getCharacteristic(this.api.hap.Characteristic.On)
       .onGet(this.getState.bind(this))   // bind to getStateHandler method below
       .onSet(this.setState.bind(this));  // bind to setStateHandler method below
+
+    this.log.info(`Command Accessory Plugin Loaded`);
   }
 
   getServices() {
@@ -36,7 +36,7 @@ class CommandAccessoryPlugin {
   }
 
   async getState() {
-    this.log.info(`Getting ${this.config.name} switch state`);
+    this.log.debug(`Getting switch state`);
 
     if (!this.config.check_status) {
       this.log.debug(`No check_status, returning static state: ${this.currentState}`)
@@ -53,12 +53,12 @@ class CommandAccessoryPlugin {
       state = false;
     }
 
-    this.log.debug(`Returning: ${state}`);
+    this.log.debug(`Returning: ${this.currentState}`);
     return state;
   }
 
   async setState(value) {
-    this.log.info(`Setting ${this.config.name} switch state to: `, value);
+    this.log.debug(`Setting switch state to: `, value);
 
     let cmd = value ? this.config.turn_on : this.config.turn_off;
     let exitCode = 1;
